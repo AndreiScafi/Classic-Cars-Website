dd8647a2169bd3200299701124335dc24489f534
Andrei Scafi <andreiscafi@hotmail.com>
2023-07-16 19:35:36 -0300

    Adding functionality to navbar menu with CSS and Javascript part two.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /* My own attempt to make the code */
 
+/* 
 const menu = document.querySelector('.menu');
 const targets = document.querySelectorAll('.target');
 
@@ -7,6 +8,22 @@ menu.addEventListener('click', () => {
     targets.forEach(target => {
         target.classList.toggle('change')
     });
-})
+}) */
 
 /* End of My own attempt to make the code */
+
+/* Teacher code */
+
+/* Menu */
+
+document.querySelector('.menu').addEventListener('click', () => {
+    document.querySelectorAll('.target').forEach((item) => {
+        item.classList.toggle('change')
+    })
+})
+
+
+/*End of  Menu */
+
+
+/* End of Teacher code */
